861268a67dc3a922313304f957c3056744253ff1
Sebastian009w <ia3266595@gmail.com>
2023-04-19 16:41:20 -0500

    blog inicial

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+import blog from "https://deno.land/x/blog@0.5.0/blog.tsx";
+
+blog({
+  title: "Berries",
+  author: "Johan Sebastian",
+  description: "making good sweets",
+  avatar: "./sebastian.jpeg",
+  avatarClass: "full",
+
+  links: [{ title: "Github", url: "https://github.com/sebastian009w" }],
+
+  favicon: "./sebastian.jpeg"
+});
