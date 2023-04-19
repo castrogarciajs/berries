0fa419b55062355310f23a6d5fa60c6c7611b2e4
Sebastian009w <ia3266595@gmail.com>
2023-04-19 16:46:25 -0500

    links de contacto

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import blog from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
-  title: "Berries",
+  title: "Johan Sebastian",
   author: "Johan Sebastian",
   description: "Making Good Sweets - MGS",
   avatar: "./sebastian.jpeg",
   avatarClass: "full",
 
-  links: [{ title: "Github", url: "https://github.com/sebastian009w" }],
+  links: [{ title: "Github", url: "https://github.com/sebastian009w" }, {title: "Twitter", url: "https://twitter.com/MmJohancs"}],
 
   favicon: "./sebastian.jpeg"
 });
